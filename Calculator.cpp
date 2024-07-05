4c00a2a7d9ddffaec0aa3f46274f05e9ff2b0903
Dorian11ro-coding <minimickey427@yahoo.com>
2024-07-05 22:53:57 +0300

    cleaned up code

--- Calculator.cpp
+++ Calculator.cpp
@@ -188,12 +188,6 @@ int main(int argc, char** argv) {
                 cout << totalCost << endl;
                 return 0;
                 
-
-                
-                
-                
-
-                
     }
     return 0;
 }
